c3d2d1835b7f4bb2a01dd80c4209d4d43ad3f140
gimhanul <gimhanuly@gmail.com>
2022-03-25 10:39:27 +0900

    SET :: 프로젝트 세팅

---
+++ src/main/java/com/bamdoliro/stupetition/StupetitionApplication.java
@@ -0,0 +1,13 @@
+package com.bamdoliro.stupetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StupetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StupetitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bamdoliro/stupetition/StupetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamdoliro.stupetition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StupetitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
