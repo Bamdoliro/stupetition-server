b69e9a81404adbb921b3ff63a24652fc12b80dd2
gimhanul <gimhanuly@gmail.com>
2022-04-13 10:05:46 +0900

    ADD :: SchoolNotFoundException (#6)
    - school not found exception 추가

--- src/main/java/com/bamdoliro/stupetition/global/error/exception/ErrorCode.java
+++ src/main/java/com/bamdoliro/stupetition/global/error/exception/ErrorCode.java
@@ -12,7 +12,9 @@ public enum ErrorCode {
     USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
     USER_ALREADY_EXISTS(422, "사용자가 이미 존재합니다."),
     PASSWORD_MISMATCH(401, "비밀번호가 틀렸습니다."),
-    USER_NOT_LOGIN(401, "로그인 해야 합니다.")
+    USER_NOT_LOGIN(401, "로그인 해야 합니다."),
+
+    SCHOOL_NOT_FOUND(404, "학교를 찾을 수 없습니다.")
     ;
 
     private final int status;
