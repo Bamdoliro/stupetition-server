796c8f061eabcc84bebfafd22852fef7d052f0f8
gimhanul <gimhanuly@gmail.com>
2022-04-24 09:54:57 +0900

    REFACTOR :: User 수정시 validation (#15)
    - User password 수정시 size vadlidation 추가
    - 지금까지는 수정시 size에 제약이 없었음 -> 보안 강화

--- src/main/java/com/bamdoliro/stupetition/domain/user/presentation/dto/request/UpdateUserPasswordRequestDto.java
+++ src/main/java/com/bamdoliro/stupetition/domain/user/presentation/dto/request/UpdateUserPasswordRequestDto.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @AllArgsConstructor
 public class UpdateUserPasswordRequestDto {
 
     @NotNull(message = "바꿀 비밀번호를 입력해 주세요.")
+    @Size(min = 8, max = 20)
     private String password;
 
     @NotNull(message = "현재 비밀번호를 입력해 주세요.")
