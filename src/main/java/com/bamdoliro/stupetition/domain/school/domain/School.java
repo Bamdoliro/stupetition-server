62669c2ffb32f83b32d5e00033806b032982d4c6
gimhanul <gimhanuly@gmail.com>
2022-06-04 19:44:51 +0900

    ADD :: School Entity 인원 수 (#30)
    - numberOfMembers column 추가
    - 숫자 더하고 빼는 method 추가

--- src/main/java/com/bamdoliro/stupetition/domain/school/domain/School.java
+++ src/main/java/com/bamdoliro/stupetition/domain/school/domain/School.java
@@ -1,9 +1,12 @@
 package com.bamdoliro.stupetition.domain.school.domain;
 
+import com.bamdoliro.stupetition.domain.user.domain.User;
 import com.bamdoliro.stupetition.global.entity.BaseTimeEntity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -23,9 +26,23 @@ public class School extends BaseTimeEntity {
     @Column(length = 20)
     private String emailDomain;
 
+    @Column
+    private int numberOfMembers;
+
+    @OneToMany(mappedBy = "user")
+    private List<User> membersOfSchool = new ArrayList<>();
+
     @Builder
     public School(String name, String emailDomain) {
         this.name = name;
         this.emailDomain = emailDomain;
+    }
+
+    public void addMember() {
+        numberOfMembers++;
+    }
+
+    public void subtractMember() {
+        numberOfMembers--;
     }
 }
