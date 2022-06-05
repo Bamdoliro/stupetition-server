521cac5dba97e4531164ee172f70e1425946f049
gimhanul <gimhanuly@gmail.com>
2022-06-05 14:31:12 +0900

    ADD :: board agreer 비율 정의 (#32)

---
+++ src/main/java/com/bamdoliro/stupetition/domain/board/domain/constant/BoardConstant.java
@@ -0,0 +1,7 @@
+package com.bamdoliro.stupetition.domain.board.domain.constant;
+
+public class BoardConstant {
+
+    // TODO :: 회의 후 비율 수정
+    public static final double BOARD_AGREER_PERCENTAGE = 0.5;
+}
