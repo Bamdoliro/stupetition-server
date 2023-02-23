90263430c94c1af2e3ae6ace67fca3e1b0eca95e
gimhanul <gimhanuly@gmail.com>
2023-02-23 19:34:47 +0900

    ADD :: error code to error response

--- src/main/java/com/bamdoliro/stupetition/global/error/ErrorResponse.java
+++ src/main/java/com/bamdoliro/stupetition/global/error/ErrorResponse.java
@@ -11,10 +11,12 @@ import lombok.Getter;
 public class ErrorResponse {
 
     private final int status;
+    private final String code;
     private final String message;
 
     public ErrorResponse(ErrorCode errorCode) {
         this.status = errorCode.getStatus();
+        this.code = errorCode.name();
         this.message = errorCode.getMessage();
     }
 }
