6b4601a3bb83ad292d02ffadfb1f8e1f3cee08a8
gimhanul <gimhanuly@gmail.com>
2023-04-09 23:43:17 +0900

    ADD :: swagger 3
    - swagger 3으로 버전 업
    - security config deprecated method 수정
    - swagger ui security config 경로 변경
    - security config 삭제 (자동으로 생성)

--- src/main/java/com/bamdoliro/stupetition/global/security/SecurityConfig.java
+++ src/main/java/com/bamdoliro/stupetition/global/security/SecurityConfig.java
@@ -15,16 +15,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsUtils;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private final AuthDetailsService authDetailsService;
     private final JwtTokenProvider jwtTokenProvider;
@@ -37,24 +39,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web ->
+                web
+                        .ignoring()
+                        .mvcMatchers("/swagger-ui/**", "/configuration/**", "/swagger-resources/**", "/v3/api-docs", "/webjars/**", "/webjars/springfox-swagger-ui/*.{js,css}");
     }
 
-    @Override
-    public void configure(WebSecurity web) {
-        web
-                .ignoring()
-                .mvcMatchers("/swagger-ui.html/**", "/configuration/**", "/swagger-resources/**", "/v2/api-docs", "/webjars/**", "/webjars/springfox-swagger-ui/*.{js,css}");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable()
                 .csrf().disable()
                 .rememberMe().disable()
+                .formLogin().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
@@ -65,16 +63,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/school/search").permitAll()
                 .antMatchers("/answer/**").hasRole("STUDENT_COUNCIL")
                 .anyRequest().authenticated()
+
                 .and()
-                .formLogin().disable();
-
-
-        http
                 .addFilterBefore(new JwtAuthenticationFilter(
                                 authDetailsService, jwtTokenProvider, jwtValidateService),
                         UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new GlobalErrorFilter(mapper), JwtAuthenticationFilter.class);
 
 
+        return http.build();
     }
 }
