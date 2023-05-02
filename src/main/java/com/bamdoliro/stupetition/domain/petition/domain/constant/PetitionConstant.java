6d4f95a834d20a9a870ff217ed26dc802667654e
gimhanul <gimhanuly@gmail.com>
2023-05-03 08:44:32 +0900

    CHORE :: 50%로 변경
    - 테스트 비율 변경해도 바꿀 필요 없도록 수정

--- src/main/java/com/bamdoliro/stupetition/domain/petition/domain/constant/PetitionConstant.java
+++ src/main/java/com/bamdoliro/stupetition/domain/petition/domain/constant/PetitionConstant.java
@@ -2,5 +2,5 @@ package com.bamdoliro.stupetition.domain.petition.domain.constant;
 
 public class PetitionConstant {
 
-    public static final double PETITION_AGREER_PERCENTAGE = 0.7;
+    public static final double PETITION_AGREER_PERCENTAGE = 0.5;
 }
